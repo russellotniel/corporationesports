a04f2e3325033a3cabe00ad4ed2ac4a8a8cc621f
russellotniel <russellotniel@gmail.com>
2021-08-20 14:10:21 +0700

    Deployed registration page along with the css and js. Deleted index page and other useless css codes.

--- js/main.js
+++ js/main.js
@@ -2,90 +2,247 @@ const scriptURL = "https://script.google.com/macros/s/AKfycbwE6IxWPlixN4l_deEUEE
 const form = document.forms["registrationForm"];
 const btnRegister = document.querySelector(".btnRegister");
 const loading = document.querySelector(".loading");
-const myAlert = document.querySelector(".myAlert");
+const paymentAlert = document.querySelector(".paymentAlert");
+const teamName = document.getElementById("teamName");
+const teamPhoneNumber = document.getElementById("teamPhoneNumber");
+const player1IGN = document.getElementById("player1IGN");
+const player2IGN = document.getElementById("player2IGN");
+const player3IGN = document.getElementById("player3IGN");
+const player4IGN = document.getElementById("player4IGN");
+const player5IGN = document.getElementById("player5IGN");
+const player6IGN = document.getElementById("player6IGN");
+const redirectError = document.querySelector(".redirectError");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	//Loading muncul, submit ilang
-	var teamPhoneNumber = form["teamPhoneNumber"].value;
-	var player1IGN = form["player1IGN"].value;
-	var player2IGN = form["player2IGN"].value;
-	var player3IGN = form["player3IGN"].value;
-	var player4IGN = form["player4IGN"].value;
-	var player5IGN = form["player5IGN"].value;
-	var player6IGN = form["player6IGN"].value;
+	//Get all the values
+	let teamNameValue = teamName.value.trim();
+	let teamPhoneNumberValue = teamPhoneNumber.value.trim();
+	let player1IGNValue = player1IGN.value.trim();
+	let player2IGNValue = player2IGN.value.trim();
+	let player3IGNValue = player3IGN.value.trim();
+	let player4IGNValue = player4IGN.value.trim();
+	let player5IGNValue = player5IGN.value.trim();
+	let player6IGNValue = player6IGN.value.trim();
 
-	if (player5IGN !== "" && player6IGN !== "") {
-		var validationIGNs = checkIGN6(player1IGN, player2IGN, player3IGN, player4IGN, player5IGN, player6IGN);
-	} else if (player5IGN !== "") {
-		var validationIGNs = checkIGN5(player1IGN, player2IGN, player3IGN, player4IGN, player5IGN);
-	} else {
-		var validationIGNs = checkIGN4(player1IGN, player2IGN, player3IGN, player4IGN);
+	//Check primary IGNs has value and maximum length
+	let validationIGN1 = checkIGN(player1IGNValue, player1IGN);
+	let validationIGN2 = checkIGN(player2IGNValue, player2IGN);
+	let validationIGN3 = checkIGN(player3IGNValue, player3IGN);
+	let validationIGN4 = checkIGN(player4IGNValue, player4IGN);
+	let validationIGN5 = checkSecondaryIGN(player5IGNValue, player5IGN);
+	let validationIGN6 = checkSecondaryIGN(player6IGNValue, player6IGN);
+	let validationIGNs = false;
+
+	//Primary IGNs validations
+	if (validationIGN1 && validationIGN2 && validationIGN3 && validationIGN4) {
+		//Secondary IGNs validations
+		if (validationIGN5 && validationIGN6) {
+			//Set array for values and duplicates
+			let array = [];
+			array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue, player5IGNValue, player6IGNValue];
+
+			//Check for duplicates
+			let duplicates = findDuplicates(array);
+
+			if (duplicates.length !== 0) {
+				for (let i = 0; i < duplicates.length; i++) {
+					for (let j = 0; j < array.length; j++) {
+						if (duplicates[i] === array[j]) {
+							let ign = "player" + (j + 1) + "IGN";
+							setErrorFor(eval(ign), "IGN tidak boleh sama.");
+						}
+					}
+				}
+			} else {
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				validationIGNs = true;
+			}
+		} else if (validationIGN5) {
+			//Set array for values and duplicates
+			let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue, player5IGNValue];
+
+			//Check for duplicates
+			let duplicates = findDuplicates(array);
+
+			if (duplicates.length !== 0) {
+				for (let i = 0; i < duplicates.length; i++) {
+					for (let j = 0; j < array.length; j++) {
+						if (duplicates[i] === array[j]) {
+							let ign = "player" + (j + 1) + "IGN";
+							setErrorFor(eval(ign), "IGN tidak boleh sama.");
+						}
+					}
+				}
+			} else {
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				validationIGNs = true;
+			}
+		}
+		//Set array for values and duplicates
+		let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue];
+
+		//Check for duplicates
+		let duplicates = findDuplicates(array);
+
+		if (duplicates.length !== 0) {
+			for (let i = 0; i < duplicates.length; i++) {
+				for (let j = 0; j < array.length; j++) {
+					if (duplicates[i] === array[j]) {
+						let ign = "player" + (j + 1) + "IGN";
+						setErrorFor(eval(ign), "IGN tidak boleh sama.");
+					}
+				}
+			}
+		} else {
+			for (let i = 0; i < array.length; i++) {
+				setSuccessFor(eval("player" + [i + 1] + "IGN"));
+			}
+			validationIGNs = true;
+		}
 	}
 
-	var validationPhone = checkPhone(teamPhoneNumber);
+	let validationTeamName = checkTeamName(teamNameValue);
+	let validationPhone = checkPhone(teamPhoneNumberValue);
 
-	if (validationPhone && validationIGNs) {
+	if (validationPhone && validationTeamName && validationIGNs) {
+		//Loading muncul, submit ilang
 		loading.classList.toggle("d-none");
 		btnRegister.classList.toggle("d-none");
 		fetch(scriptURL, { method: "POST", body: new FormData(form) })
 			.then((response) => {
-				// Loading ilang, submit muncul
+				// Loading ilang
 				loading.classList.toggle("d-none");
-				btnRegister.classList.toggle("d-none");
-				// Alert muncul
-				myAlert.classList.toggle("d-none");
-				//Reset Form
+				// Reset Form
+				validationIGNs = false;
 				form.reset();
+				// Reset Classes
+				resetClass(teamName);
+				resetClass(teamPhoneNumber);
+				resetClass(player1IGN);
+				resetClass(player2IGN);
+				resetClass(player3IGN);
+				resetClass(player4IGN);
+				resetClass(player5IGN);
+				resetClass(player6IGN);
+
+				// Redirect Error Message
+				redirectError.classList.toggle("d-none");
+				// Payment Alert ilang
+				// paymentAlert.classList.toggle("d-none");
+				// Redirect to WA
+				alert("Anda akan dialihkan untuk chat admin WA kami untuk memberikan bukti pembayaran. Pastikan pembayaran sudah dilakukan.");
+				window.location.assign("https://wa.me/6288212599372?text=Nama%20Tim:%0AIni%20bukti%20pembayarannya%0ATerima%20Kasih!");
+
 				console.log("Success!", response);
 			})
-			.catch((error) => console.error("Error!", error.message));
-	} else {
-		alert("Silahkan isi ulang form ini.");
-		form.reset();
+			.catch((error) => {
+				console.error("Error!", error.message);
+				alert("Terjadi sebuah masalah. Silahkan refresh page ini dan isi ulang form!");
+				form.reset();
+				// Reset Classes
+				resetClass(teamName);
+				resetClass(teamPhoneNumber);
+				resetClass(player1IGN);
+				resetClass(player2IGN);
+				resetClass(player3IGN);
+				resetClass(player4IGN);
+				resetClass(player5IGN);
+				resetClass(player6IGN);
+				// Loading ilang
+				loading.classList.toggle("d-none");
+			});
 	}
 });
 
-function checkPhone(phone) {
-	if (phone.startsWith("08", 0)) {
-		if (phone.length == 10 || phone.length == 12) {
-			return true;
+// document.querySelector(".btnKirimBukti").addEventListener("click", function () {
+// 	// Alert sama Register muncul
+// 	myAlert.classList.toggle("d-none");
+// 	btnRegister.classList.toggle("d-none");
+// 	// Payment ilang
+// 	payment.classList.toggle("d-none");
+// });
+
+function checkTeamName(teamNameValue) {
+	if (teamNameValue === "") {
+		setErrorFor(teamName, "Silahkan isi nama tim kalian.");
+		return false;
+	} else {
+		setSuccessFor(teamName);
+		return true;
+	}
+}
+
+function findDuplicates(array) {
+	const duplicates = [];
+	const compare = [];
+
+	for (let i = 0; i < array.length; i++) {
+		if (!compare.includes(array[i])) {
+			compare.push(array[i]);
 		} else {
-			alert("Nomor HP harus 10 atau 12 angka.");
+			duplicates.push(array[i]);
+		}
+	}
+	return duplicates;
+}
+
+function checkPhone(teamPhoneNumberValue) {
+	if (teamPhoneNumberValue === "") {
+		setErrorFor(teamPhoneNumber, "Silahkan isi nomor HP.");
+		return false;
+	} else if (teamPhoneNumberValue.startsWith("08", 0)) {
+		if (teamPhoneNumberValue.length < 10 || teamPhoneNumberValue.length > 12) {
+			setErrorFor(teamPhoneNumber, "Nomor HP harus 10 sampai 12 angka.");
 			return false;
+		} else {
+			setSuccessFor(teamPhoneNumber);
+			return true;
 		}
 	} else {
-		alert("Nomor HP harus dimulai dengan '08'");
+		setErrorFor(teamPhoneNumber, "Nomor HP harus dimulai dengan '08'.");
+	}
+}
+
+function checkIGN(playerIGNValue, playerIGN) {
+	if (playerIGNValue === "") {
+		setErrorFor(playerIGN, "Silahkan isi IGN.");
+		return false;
+	} else if (playerIGNValue.length > 30) {
+		setErrorFor(playerIGN, "IGN tidak boleh lebih dari 30 characters.");
 		return false;
 	}
+	return true;
 }
 
-function checkIGN4(player1IGN, player2IGN, player3IGN, player4IGN) {
-	const IGNs = [player1IGN, player2IGN, player3IGN, player4IGN];
-	for (let i = 0; i < IGNs.length; i++) {
-		if (IGNs[i] === IGNs[i + 1]) {
-			alert("IGN tidak boleh sama. Silahkan diganti. 4");
-			break;
-		}
+function checkSecondaryIGN(playerIGNValue, playerIGN) {
+	if (playerIGNValue.length > 30) {
+		setErrorFor(playerIGN, "IGN tidak boleh lebih dari 30 characters.");
+		return false;
+	} else if (playerIGNValue === "") {
+		return false;
 	}
+	return true;
 }
 
-function checkIGN5(player1IGN, player2IGN, player3IGN, player4IGN, player5IGN) {
-	const IGNs = [player1IGN, player2IGN, player3IGN, player4IGN, player5IGN];
-	for (let i = 0; i < IGNs.length; i++) {
-		if (IGNs[i] === IGNs[i + 1]) {
-			alert("IGN tidak boleh sama. Silahkan diganti. 5");
-			break;
-		}
-	}
+function setErrorFor(input, message) {
+	const formDiv = input.parentElement;
+	const small = formDiv.querySelector("small");
+
+	small.innerText = message;
+	formDiv.className = "formDiv error";
 }
 
-function checkIGN6(player1IGN, player2IGN, player3IGN, player4IGN, player5IGN, player6IGN) {
-	const IGNs = [player1IGN, player2IGN, player3IGN, player4IGN, player5IGN, player6IGN];
-	for (let i = 0; i < IGNs.length; i++) {
-		if (IGNs[i] === IGNs[i + 1]) {
-			alert("IGN tidak boleh sama. Silahkan diganti. 6");
-			break;
-		}
-	}
+function setSuccessFor(input) {
+	const formDiv = input.parentElement;
+	formDiv.className = "formDiv success";
+}
+
+function resetClass(input) {
+	const formDiv = input.parentElement;
+	formDiv.classList.remove("success");
 }
