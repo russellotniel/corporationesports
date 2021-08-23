3f6f89cd40f0d143730dd4d1beeb63bff2eba5ab
russellotniel <russellotniel@gmail.com>
2021-08-23 17:03:21 +0700

    Fixed javascript with more validations (length of IGN and more).

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ const redirectError = document.querySelector(".redirectError");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	//Get all the values
+
+	// Get all the values
 	let teamNameValue = teamName.value.trim();
 	let teamPhoneNumberValue = teamPhoneNumber.value.trim();
 	let player1IGNValue = player1IGN.value.trim();
@@ -25,27 +26,56 @@ form.addEventListener("submit", (e) => {
 	let player5IGNValue = player5IGN.value.trim();
 	let player6IGNValue = player6IGN.value.trim();
 
-	//Check primary IGNs has value and maximum length
-	let validationIGN1 = checkIGN(player1IGNValue, player1IGN);
-	let validationIGN2 = checkIGN(player2IGNValue, player2IGN);
-	let validationIGN3 = checkIGN(player3IGNValue, player3IGN);
-	let validationIGN4 = checkIGN(player4IGNValue, player4IGN);
-	let validationIGN5 = checkSecondaryIGN(player5IGNValue, player5IGN);
-	let validationIGN6 = checkSecondaryIGN(player6IGNValue, player6IGN);
-	let validationIGNs = false;
+	// Check primary IGNs has value and maximum length
+	let validationIGN1 = checkPrimaryIGN(player1IGNValue, player1IGN);
+	let validationIGN2 = checkPrimaryIGN(player2IGNValue, player2IGN);
+	let validationIGN3 = checkPrimaryIGN(player3IGNValue, player3IGN);
+	let validationIGN4 = checkPrimaryIGN(player4IGNValue, player4IGN);
+	let validationIGN5 = checkSecondaryIGN(player5IGNValue);
+	let validationIGN6 = checkSecondaryIGN(player6IGNValue);
 
-	//Primary IGNs validations
+	// If 5 and 6 has inputs but deleted.
+	if (!validationIGN5) {
+		resetClass(player5IGN);
+	}
+	if (!validationIGN6) {
+		resetClass(player6IGN);
+	}
+
+	// Primary Validations
+	let validationIGNs = false;
+	let validationTeamName = checkTeamName(teamNameValue);
+	let validationPhone = checkPhone(teamPhoneNumberValue);
+
+	// Primary IGNs validations
 	if (validationIGN1 && validationIGN2 && validationIGN3 && validationIGN4) {
+		// Check for length 3 - 30
+		let IGN1Details = checkIGNDetails(player1IGNValue, player1IGN);
+		let IGN2Details = checkIGNDetails(player2IGNValue, player2IGN);
+		let IGN3Details = checkIGNDetails(player3IGNValue, player3IGN);
+		let IGN4Details = checkIGNDetails(player4IGNValue, player4IGN);
+
 		//Secondary IGNs validations
 		if (validationIGN5 && validationIGN6) {
-			//Set array for values and duplicates
-			let array = [];
-			array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue, player5IGNValue, player6IGNValue];
+			// Check for length 3 - 30
+			let IGN5Details = checkIGNDetails(player5IGNValue, player5IGN);
+			let IGN6Details = checkIGNDetails(player6IGNValue, player6IGN);
 
-			//Check for duplicates
+			//Set array for values and duplicates
+			let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue, player5IGNValue, player6IGNValue];
+
+			// Check for duplicates
 			let duplicates = findDuplicates(array);
 
+			// Validation to check if 5 and 6 are valid
+			let gate5n6 = false;
+
 			if (duplicates.length !== 0) {
+				// Set all to success first
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				// Set the duplicates to error
 				for (let i = 0; i < duplicates.length; i++) {
 					for (let j = 0; j < array.length; j++) {
 						if (duplicates[i] === array[j]) {
@@ -55,19 +85,43 @@ form.addEventListener("submit", (e) => {
 					}
 				}
 			} else {
+				// If there are no duplicates, set all to success and continue
 				for (let i = 0; i < array.length; i++) {
 					setSuccessFor(eval("player" + [i + 1] + "IGN"));
 				}
+				gate5n6 = true;
+			}
+
+			if (IGN1Details && IGN2Details && IGN3Details && IGN4Details && IGN5Details && IGN6Details && gate5n6) {
 				validationIGNs = true;
+			} else {
+				// Send Error message
+				checkIGNDetails(player1IGNValue, player1IGN);
+				checkIGNDetails(player2IGNValue, player2IGN);
+				checkIGNDetails(player3IGNValue, player3IGN);
+				checkIGNDetails(player4IGNValue, player4IGN);
+				checkIGNDetails(player5IGNValue, player5IGN);
+				checkIGNDetails(player6IGNValue, player6IGN);
 			}
 		} else if (validationIGN5) {
+			// Check for length 3 - 30
+			let IGN5Details = checkIGNDetails(player5IGNValue, player5IGN);
+
 			//Set array for values and duplicates
 			let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue, player5IGNValue];
 
-			//Check for duplicates
+			// Check for duplicates
 			let duplicates = findDuplicates(array);
 
+			// Validation to check if 5 is valid
+			let gate5 = false;
+
 			if (duplicates.length !== 0) {
+				// Set all to success first
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				// Set the duplicates to error
 				for (let i = 0; i < duplicates.length; i++) {
 					for (let j = 0; j < array.length; j++) {
 						if (duplicates[i] === array[j]) {
@@ -77,42 +131,72 @@ form.addEventListener("submit", (e) => {
 					}
 				}
 			} else {
+				// If there are no duplicates, set all to success and continue
 				for (let i = 0; i < array.length; i++) {
 					setSuccessFor(eval("player" + [i + 1] + "IGN"));
 				}
-				validationIGNs = true;
+				gate5 = true;
 			}
-		}
-		//Set array for values and duplicates
-		let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue];
 
-		//Check for duplicates
-		let duplicates = findDuplicates(array);
-
-		if (duplicates.length !== 0) {
-			for (let i = 0; i < duplicates.length; i++) {
-				for (let j = 0; j < array.length; j++) {
-					if (duplicates[i] === array[j]) {
-						let ign = "player" + (j + 1) + "IGN";
-						setErrorFor(eval(ign), "IGN tidak boleh sama.");
-					}
-				}
+			if (IGN1Details && IGN2Details && IGN3Details && IGN4Details && IGN5Details && gate5) {
+				validationIGNs = true;
+			} else {
+				// Send Error message
+				checkIGNDetails(player1IGNValue, player1IGN);
+				checkIGNDetails(player2IGNValue, player2IGN);
+				checkIGNDetails(player3IGNValue, player3IGN);
+				checkIGNDetails(player4IGNValue, player4IGN);
+				checkIGNDetails(player5IGNValue, player5IGN);
 			}
 		} else {
-			for (let i = 0; i < array.length; i++) {
-				setSuccessFor(eval("player" + [i + 1] + "IGN"));
+			//Set array for values and duplicates
+			let array = [player1IGNValue, player2IGNValue, player3IGNValue, player4IGNValue];
+
+			//Check for duplicates
+			let duplicates = findDuplicates(array);
+
+			let gate4 = false;
+
+			if (duplicates.length !== 0) {
+				alert("duplicates");
+				// Set all to success first
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				// Set duplicates to error
+				for (let i = 0; i < duplicates.length; i++) {
+					for (let j = 0; j < array.length; j++) {
+						if (duplicates[i] === array[j]) {
+							let ign = "player" + (j + 1) + "IGN";
+							setErrorFor(eval(ign), "IGN tidak boleh sama.");
+						}
+					}
+				}
+			} else {
+				alert("no duplicates");
+				// If there are no duplicates, set all to success and continue
+				for (let i = 0; i < array.length; i++) {
+					setSuccessFor(eval("player" + [i + 1] + "IGN"));
+				}
+				gate4 = true;
 			}
-			validationIGNs = true;
+			if (IGN1Details && IGN2Details && IGN3Details && IGN4Details && gate4) {
+				validationIGNs = true;
+			} else {
+				// Send error message
+				checkIGNDetails(player1IGNValue, player1IGN);
+				checkIGNDetails(player2IGNValue, player2IGN);
+				checkIGNDetails(player3IGNValue, player3IGN);
+				checkIGNDetails(player4IGNValue, player4IGN);
+			}
 		}
 	}
-
-	let validationTeamName = checkTeamName(teamNameValue);
-	let validationPhone = checkPhone(teamPhoneNumberValue);
 
 	if (validationPhone && validationTeamName && validationIGNs) {
 		//Loading muncul, submit ilang
 		loading.classList.toggle("d-none");
 		btnRegister.classList.toggle("d-none");
+
 		fetch(scriptURL, { method: "POST", body: new FormData(form) })
 			.then((response) => {
 				// Loading ilang
@@ -132,8 +216,7 @@ form.addEventListener("submit", (e) => {
 
 				// Redirect Error Message
 				redirectError.classList.toggle("d-none");
-				// Payment Alert ilang
-				// paymentAlert.classList.toggle("d-none");
+
 				// Redirect to WA
 				alert("Anda akan dialihkan untuk chat admin WA kami untuk memberikan bukti pembayaran. Pastikan pembayaran sudah dilakukan.");
 				window.location.assign("https://wa.me/6288212599372?text=Nama%20Tim:%0AIni%20bukti%20pembayarannya%0ATerima%20Kasih!");
@@ -141,9 +224,14 @@ form.addEventListener("submit", (e) => {
 				console.log("Success!", response);
 			})
 			.catch((error) => {
+				// Loading ilang
+				loading.classList.toggle("d-none");
 				console.error("Error!", error.message);
+
+				// Reset form
 				alert("Terjadi sebuah masalah. Silahkan refresh page ini dan isi ulang form!");
 				form.reset();
+
 				// Reset Classes
 				resetClass(teamName);
 				resetClass(teamPhoneNumber);
@@ -153,19 +241,9 @@ form.addEventListener("submit", (e) => {
 				resetClass(player4IGN);
 				resetClass(player5IGN);
 				resetClass(player6IGN);
-				// Loading ilang
-				loading.classList.toggle("d-none");
 			});
 	}
 });
-
-// document.querySelector(".btnKirimBukti").addEventListener("click", function () {
-// 	// Alert sama Register muncul
-// 	myAlert.classList.toggle("d-none");
-// 	btnRegister.classList.toggle("d-none");
-// 	// Payment ilang
-// 	payment.classList.toggle("d-none");
-// });
 
 function checkTeamName(teamNameValue) {
 	if (teamNameValue === "") {
@@ -175,20 +253,6 @@ function checkTeamName(teamNameValue) {
 		setSuccessFor(teamName);
 		return true;
 	}
-}
-
-function findDuplicates(array) {
-	const duplicates = [];
-	const compare = [];
-
-	for (let i = 0; i < array.length; i++) {
-		if (!compare.includes(array[i])) {
-			compare.push(array[i]);
-		} else {
-			duplicates.push(array[i]);
-		}
-	}
-	return duplicates;
 }
 
 function checkPhone(teamPhoneNumberValue) {
@@ -208,31 +272,45 @@ function checkPhone(teamPhoneNumberValue) {
 	}
 }
 
-function checkIGN(playerIGNValue, playerIGN) {
+function checkPrimaryIGN(playerIGNValue, playerIGN) {
 	if (playerIGNValue === "") {
 		setErrorFor(playerIGN, "Silahkan isi IGN.");
-		return false;
-	} else if (playerIGNValue.length > 30) {
-		setErrorFor(playerIGN, "IGN tidak boleh lebih dari 30 characters.");
-		return false;
-	} else if (playerIGNValue.length < 3) {
-		setErrorFor(playerIGN, "IGN tidak boleh kurang dari 3 characters.");
 		return false;
 	}
 	return true;
 }
 
-function checkSecondaryIGN(playerIGNValue, playerIGN) {
+function checkSecondaryIGN(playerIGNValue) {
+	if (playerIGNValue === "") {
+		return false;
+	}
+	return true;
+}
+
+function checkIGNDetails(playerIGNValue, playerIGN) {
 	if (playerIGNValue.length > 30) {
 		setErrorFor(playerIGN, "IGN tidak boleh lebih dari 30 characters.");
-		return false;
-	} else if (playerIGNValue === "") {
 		return false;
 	} else if (playerIGNValue.length < 3) {
 		setErrorFor(playerIGN, "IGN tidak boleh kurang dari 3 characters.");
 		return false;
 	}
+
 	return true;
+}
+
+function findDuplicates(array) {
+	const duplicates = [];
+	const compare = [];
+
+	for (let i = 0; i < array.length; i++) {
+		if (!compare.includes(array[i])) {
+			compare.push(array[i]);
+		} else {
+			duplicates.push(array[i]);
+		}
+	}
+	return duplicates;
 }
 
 function setErrorFor(input, message) {
@@ -251,4 +329,5 @@ function setSuccessFor(input) {
 function resetClass(input) {
 	const formDiv = input.parentElement;
 	formDiv.classList.remove("success");
+	formDiv.classList.remove("error");
 }
